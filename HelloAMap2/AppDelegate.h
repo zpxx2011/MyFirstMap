29c954c0354d3e24662dbaa37f723d8c71f801c5
Devin <uranaisakai@zhanyudemacbook-pro.local>
2015-11-05 14:07:32 +0800

    Initial Commit

---
+++ HelloAMap2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloAMap2
+//
+//  Created by Devin on 15/11/5.
+//  Copyright © 2015年 Devin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloAMap2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloAMap2
+//
+//  Created by Devin on 15/11/5.
+//  Copyright © 2015年 Devin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
